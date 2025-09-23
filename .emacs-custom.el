86a8ff729fc04629a7473d04c6c161cb65c5ee3a
Renato Lucindo <lucindo@gmail.com>
2025-09-23 12:14:55 -0300

    added ztree and dirvish for testing

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -4,8 +4,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
   '(package-selected-packages
-     '(company corfu-terminal embark-consult expand-region magit marginalia orderless
-	vertico)))
+     '(company corfu-terminal dirvish embark-consult expand-region magit marginalia
+	orderless vertico ztree)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
