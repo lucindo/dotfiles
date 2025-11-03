790778ebb36e080a03c42464342e27ef1d0f152d
Renato Lucindo <lucindo@gmail.com>
2025-11-03 01:26:33 -0300

    added yaml-mode

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -5,9 +5,8 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
   '(package-selected-packages
-     '(diminish embark-consult
-	expand-region golden-ratio magit marginalia orderless vertico
-	whole-line-or-region)))
+     '(diminish embark-consult expand-region flymake-yaml golden-ratio magit
+	marginalia orderless vertico whole-line-or-region yaml-mode)))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
