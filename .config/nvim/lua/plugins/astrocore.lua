ffb68078a131960e18dd24811c76909ccca8086c
Renato Lucindo <lucindo@gmail.com>
2025-11-19 12:33:11 -0300

    cleanup

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -43,7 +43,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
-        colorcolumn = "80",
+        colorcolumn = "80", -- sets vim.opt.colorcolumn
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
