f5c7f5ed75e443cb503dc9f122d5ccec8f55d393
Renato Lucindo <lucindo@gmail.com>
2025-11-20 15:35:29 -0300

    nvim: picker & dashboard

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -18,20 +18,46 @@ return {
 
   -- customize dashboard options
   {
+    -- Picker change based on https://www.youtube.com/watch?v=kR7PfBLWm6o
+    -- Dashbord from example at https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md
     "folke/snacks.nvim",
     opts = {
       dashboard = {
-        preset = {
-          header = table.concat({
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-          }, "\n"),
+        formats = {
+          key = function(item) return { { "[", hl = "special" }, { item.key, hl = "key" }, { "]", hl = "special" } } end,
+        },
+        sections = {
+          { section = "terminal", cmd = "fortune -s | cowsay", hl = "header", padding = 1, indent = 8 },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
         },
       },
       indent = { enabled = false },
+      picker = {
+        layout = "custom", -- "ivy"
+        layouts = {
+          custom = {
+            layout = {
+              box = "vertical",
+              backdrop = false,
+              row = -1,
+              width = 0,
+              height = 0.4,
+              border = "none",
+              title = " {title} {live} {flags}",
+              title_pos = "left",
+              {
+                box = "horizontal",
+                { win = "list", border = "rounded" },
+                { win = "preview", title = "{preview}", width = 0.6, border = "rounded" },
+              },
+              { win = "input", height = 1, border = "bottom" },
+            },
+          },
+        },
+      },
     },
   },
 
