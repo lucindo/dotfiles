3650073017c63904b141eabdf1411ea6002ade29
Renato Lucindo <lucindo@gmail.com>
2025-11-20 02:33:35 -0300

    leap-nvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -10,10 +10,10 @@ return {
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.bash" },
   -- theme & looks
   { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   -- navigation
   { import = "astrocommunity.recipes.heirline-tabline-buffer-number" },
+  { import = "astrocommunity.motion.leap-nvim" },
 }
