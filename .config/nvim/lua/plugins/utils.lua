9339b721912a72b5b7c2f85ac24a517d9a787e86
Renato Lucindo <lucindo@gmail.com>
2025-11-27 17:17:46 -0300

    added fugitive back and keymaps for git

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -200,7 +200,18 @@ return {
   {
     -- Git support
     "tpope/vim-fugitive",
-    config = function() vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" }) end,
+    config = function()
+      local telescope = require "telescope.builtin"
+      vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [s]tatus" })
+      vim.keymap.set("n", "<leader>gB", telescope.git_branches, { desc = "[G]it [B]ranches (Search)" })
+      vim.keymap.set("n", "<leader>gH", telescope.git_commits, { desc = "[G]it commit [H]istory (Search)" })
+      vim.keymap.set("n", "<leader>gh", telescope.git_bcommits, { desc = "[G]it commit [h]istory (Buffer)" })
+      vim.keymap.set("n", "<leader>gf", telescope.git_files, { desc = "[G]it [f]ind (Search git files)" })
+      vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit, { desc = "[G]it [d]iff" })
+      vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite, { desc = "[G]it [a]dd" })
+      vim.keymap.set("n", "<leader>gc", function() vim.cmd "Git commit" end, { desc = "[G]it [c]ommit" })
+      vim.keymap.set("n", "<leader>gl", function() vim.cmd "Git log" end, { desc = "[G]it [l]log" })
+    end,
   },
   {
     -- Better terminal support
