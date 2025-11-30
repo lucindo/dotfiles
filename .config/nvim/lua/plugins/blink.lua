39bdbc6d8ad434618f81b18b975b0bf42ec03225
Renato Lucindo <lucindo@gmail.com>
2025-11-30 17:57:22 -0300

    lspkind working on blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -6,6 +6,8 @@ return {
 	dependencies = {
 		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
 		"rafamadriz/friendly-snippets",
+		"nvim-mini/mini.nvim",
+		"onsails/lspkind.nvim", -- VS Code–style pictograms
 	},
 	opts = {
 		keymap = {
@@ -24,6 +26,48 @@ return {
 				draw = {
 					treesitter = { "lsp" },
 					columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local mini_icon, _ = require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+									if mini_icon then
+										return mini_icon .. ctx.icon_gap
+									end
+								end
+
+								local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
+								return icon .. ctx.icon_gap
+							end,
+
+							-- Optionally, use the highlight groups from mini.icons
+							-- You can also add the same function for `kind.highlight` if you want to
+							-- keep the highlight groups in sync with the icons.
+							highlight = function(ctx)
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local mini_icon, mini_hl =
+										require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+									if mini_icon then
+										return mini_hl
+									end
+								end
+								return ctx.kind_hl
+							end,
+						},
+						kind = {
+							-- Optional, use highlights from mini.icons
+							highlight = function(ctx)
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local mini_icon, mini_hl =
+										require("mini.icons").get_icon(ctx.item.data.type, ctx.label)
+									if mini_icon then
+										return mini_hl
+									end
+								end
+								return ctx.kind_hl
+							end,
+						},
+					},
 				},
 			},
 			documentation = { auto_show = true },
