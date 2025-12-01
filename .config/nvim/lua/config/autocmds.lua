2d0faa6fe7be6465b724ec7bbcbfedeabc2a5caa
Renato Lucindo <lucindo@gmail.com>
2025-11-30 23:37:16 -0300

    cleanup

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -31,14 +31,6 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	end,
 })
 
--- show cursorline only in active window enable
-vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
-	callback = function()
-		vim.opt_local.cursorline = true
-	end,
-})
-
 -- auto resize splits when the terminal's window is resized
 vim.api.nvim_create_autocmd("VimResized", {
 	command = "wincmd =",
@@ -49,14 +41,6 @@ vim.api.nvim_create_autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
 	callback = function()
 		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-	end,
-})
-
--- show cursorline only in active window disable
-vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-	group = "active_cursorline",
-	callback = function()
-		vim.opt_local.cursorline = false
 	end,
 })
 
