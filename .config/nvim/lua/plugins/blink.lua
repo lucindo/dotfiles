c7c682543f58f7302faa5e0e37ed1dc1f653174d
Renato Lucindo <lucindo@gmail.com>
2026-01-19 11:23:28 -0300

    fix: adjusted breaking change from lspkind

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -34,7 +34,8 @@ return {
 									end
 								end
 
-								local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
+								--local icon = require("lspkind").symbolic(ctx.kind, { mode = "symbol" })
+								local icon = require("lspkind").symbol_map[ctx.kind] or ""
 								return icon .. ctx.icon_gap
 							end,
 
