30d67b9912a4c9de8b7d4d0dadfd125035b1340c
Renato Lucindo <lucindo@gmail.com>
2026-03-24 12:09:03 -0300

    added plugin resolve.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,30 +1,37 @@
 return {
-	"tpope/vim-fugitive",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-		"lewis6991/gitsigns.nvim",
+	{
+		"tpope/vim-fugitive",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"lewis6991/gitsigns.nvim",
+		},
+		config = function()
+			local telescope = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [s]tatus" })
+			vim.keymap.set("n", "<leader>gb", telescope.git_branches, { desc = "[G]it [B]ranches (Search)" })
+			vim.keymap.set("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "[G]it [B]lame" })
+			vim.keymap.set("n", "<leader>gH", telescope.git_commits, { desc = "[G]it commit [H]istory (Search)" })
+			vim.keymap.set("n", "<leader>gh", telescope.git_bcommits, { desc = "[G]it commit [h]istory (Buffer)" })
+			vim.keymap.set("n", "<leader>gf", telescope.git_files, { desc = "[G]it [f]ind (Search git files)" })
+			vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit, { desc = "[G]it [d]iff" })
+			vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite, { desc = "[G]it [a]dd" })
+			vim.keymap.set("n", "<leader>gC", function()
+				vim.cmd("Git commit")
+			end, { desc = "[G]it [C]ommit" })
+			vim.keymap.set("n", "<leader>gp", function()
+				vim.cmd("Git push")
+			end, { desc = "[G]it [p]ush" })
+			vim.keymap.set("n", "<leader>gP", function()
+				vim.cmd("Git pull")
+			end, { desc = "[G]it [P]ull" })
+			vim.keymap.set("n", "<leader>gl", function()
+				vim.cmd("Git log")
+			end, { desc = "[G]it [l]log" })
+		end,
 	},
-	config = function()
-		local telescope = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [s]tatus" })
-		vim.keymap.set("n", "<leader>gb", telescope.git_branches, { desc = "[G]it [B]ranches (Search)" })
-		vim.keymap.set("n", "<leader>gB", ":Gitsigns blame<CR>", { desc = "[G]it [B]lame" })
-		vim.keymap.set("n", "<leader>gH", telescope.git_commits, { desc = "[G]it commit [H]istory (Search)" })
-		vim.keymap.set("n", "<leader>gh", telescope.git_bcommits, { desc = "[G]it commit [h]istory (Buffer)" })
-		vim.keymap.set("n", "<leader>gf", telescope.git_files, { desc = "[G]it [f]ind (Search git files)" })
-		vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiffsplit, { desc = "[G]it [d]iff" })
-		vim.keymap.set("n", "<leader>ga", vim.cmd.Gwrite, { desc = "[G]it [a]dd" })
-		vim.keymap.set("n", "<leader>gc", function()
-			vim.cmd("Git commit")
-		end, { desc = "[G]it [c]ommit" })
-		vim.keymap.set("n", "<leader>gp", function()
-			vim.cmd("Git push")
-		end, { desc = "[G]it [p]ush" })
-		vim.keymap.set("n", "<leader>gP", function()
-			vim.cmd("Git pull")
-		end, { desc = "[G]it [P]ull" })
-		vim.keymap.set("n", "<leader>gl", function()
-			vim.cmd("Git log")
-		end, { desc = "[G]it [l]log" })
-	end,
+	{
+		"spacedentist/resolve.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
 }
