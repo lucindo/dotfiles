c8e7e61ebd0c43224209425fcfb0c98a1971cd97
Renato Lucindo <lucindo@gmail.com>
2026-04-12 02:05:30 -0300

    trying to use only default vim motions

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -8,24 +8,24 @@ return {
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
 		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
+		-- require("mini.ai").setup({ n_lines = 500 })
 
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
 		--
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require("mini.surround").setup({
-			-- mappings = {
-			--   add = "sa",
-			--   delete = "sd",
-			--   find = "gsf",
-			--   find_left = "gsF",
-			--   highlight = "gsh",
-			--   replace = "gsr",
-			--   update_n_lines = "gsn",
-			-- },
-		})
+		-- require("mini.surround").setup({
+		-- 	-- mappings = {
+		-- 	--   add = "sa",
+		-- 	--   delete = "sd",
+		-- 	--   find = "gsf",
+		-- 	--   find_left = "gsF",
+		-- 	--   highlight = "gsh",
+		-- 	--   replace = "gsr",
+		-- 	--   update_n_lines = "gsn",
+		-- 	-- },
+		-- })
 
 		-- Better comment support
 		--
