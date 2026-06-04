ba8c9089bfed5ddfd12deb10b881ff0b5c98753a
Renato Lucindo <lucindo@gmail.com>
2026-06-04 18:45:44 -0300

    feat(nvim): treesitter textobjects in mini.ai, surround on gs
    mini.ai gains @function/@class/@parameter/@block objects via
    nvim-treesitter-textobjects queries (af/if, ac/ic, aa/ia, ao/io).
    mini.surround enabled on the gs prefix so flash keeps bare s.

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,45 +1,63 @@
 return {
-	"nvim-mini/mini.nvim",
-	version = "*",
-	config = function()
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-		--  - ci'  - [C]hange [I]nside [']quote
-		-- require("mini.ai").setup({ n_lines = 500 })
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main", -- provides @function/@class/@loop/... queries for mini.ai
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+	},
+	{
+		"nvim-mini/mini.nvim",
+		version = "*",
+		dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+		config = function()
+			-- Around/Inside textobjects, treesitter-aware.
+			--  - vaf / dif - [A]round / [I]nside [F]unction
+			--  - vac / dic - [C]lass
+			--  - daa / cia - [A]rgument (parameter)
+			--  - vao / dio - code block ([O]: if/for/while)
+			-- Falls back to mini.ai builtins for ), ', ", t (tag), etc.
+			local ai = require("mini.ai")
+			ai.setup({
+				n_lines = 500,
+				custom_textobjects = {
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+					a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }),
+					o = ai.gen_spec.treesitter({
+						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+					}),
+				},
+			})
 
-		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
-		-- require("mini.surround").setup({
-		-- 	-- mappings = {
-		-- 	--   add = "sa",
-		-- 	--   delete = "sd",
-		-- 	--   find = "gsf",
-		-- 	--   find_left = "gsF",
-		-- 	--   highlight = "gsh",
-		-- 	--   replace = "gsr",
-		-- 	--   update_n_lines = "gsn",
-		-- 	-- },
-		-- })
+			-- Add/delete/replace surroundings. On `gs` prefix so flash keeps bare `s`.
+			--  - gsaiw) - [A]dd surround [I]nner [W]ord with )
+			--  - gsd'   - [D]elete surrounding '
+			--  - gsr)'  - [R]eplace ) with '
+			--  - gsaif) - surround [I]nner [F]unction with ) (uses the mini.ai object)
+			require("mini.surround").setup({
+				mappings = {
+					add = "gsa",
+					delete = "gsd",
+					find = "gsf",
+					find_left = "gsF",
+					highlight = "gsh",
+					replace = "gsr",
+					update_n_lines = "gsn",
+				},
+			})
 
-		-- Better comment support
-		--
-		--  - gc   - Toggle comment (works like gcip) for visual and normal modes
-		--  - gcc  - Toggle comment for the current line
-		--  - gc   - Toggle comment on visual selection
-		require("mini.comment").setup()
+			-- Better comment support
+			--  - gc   - Toggle comment (works like gcip) for visual and normal modes
+			--  - gcc  - Toggle comment for the current line
+			require("mini.comment").setup()
 
-		-- Stauts line
-		local statusline = require("mini.statusline")
-		statusline.setup({ use_icons = false })
-		---@diagnostic disable-next-line: duplicate-set-field
-		statusline.section_location = function()
-			return "%2l:%-2v"
-		end
-	end,
+			-- Status line
+			local statusline = require("mini.statusline")
+			statusline.setup({ use_icons = false })
+			---@diagnostic disable-next-line: duplicate-set-field
+			statusline.section_location = function()
+				return "%2l:%-2v"
+			end
+		end,
+	},
 }
