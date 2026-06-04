601d3997df1780238c994c489e14e06a6d256ab7
Renato Lucindo <lucindo@gmail.com>
2026-06-04 17:35:52 -0300

    refactor(nvim): drop dead training-wheels arrow-key block

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -99,10 +99,3 @@ vim.g.disable_autoformat = false -- Disable formatting file on saving
 vim.g.finder_on_open = true -- Open a fuzzy finder floater if nvim starts with no file
 vim.g.code_context = true -- Enable Treesitter context (UI)
 vim.g.enable_avante = false -- Enable Avante (required adicional setup)
-
-if false and vim.g.training_wheels then
-	vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-	vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-	vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-	vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-end
